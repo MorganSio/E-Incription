28568a1e571a961b0b005646c6bcec2d9062b6ef
MorganSio <161039598+MorganSio@users.noreply.github.com>
2024-10-07 14:35:18 +0200

    first commit avant ORM

---
+++ src/Controller/FormulaireMDLController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FormulaireMDLController extends AbstractController
+{
+    #[Route('/formulaire_mdl', name: 'formulaire_mdl')]
+    public function index(): Response
+    {
+        return $this->render('index/formulaire_mdl.html.twig', [
+            'controller_name' => 'FormulaireMDLController',
+        ]);
+    }
+}
