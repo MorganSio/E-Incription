7f232c3c3f258dd92114ab27ae4c80f1931c3d58
MorganSio <161039598+MorganSio@users.noreply.github.com>
2024-11-15 12:26:10 +0100

    "Removed duplicate class definitions in IndexController.php and Kernel.php"

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,20 +14,3 @@ class IndexController extends AbstractController
         return $this->render('index/index.html.twig', []);
     }
 }
-
-<?php
-
-namespace App\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-
-class IndexController extends AbstractController
-{
-    #[Route('/', name: 'index')]
-    public function index(): Response
-    {
-        return $this->render('index/index.html.twig', []);
-    }
-}
--- src/Kernel.php
+++ src/Kernel.php
@@ -9,15 +9,3 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 }
-
-<?php
-
-namespace App;
-
-use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-
-class Kernel extends BaseKernel
-{
-    use MicroKernelTrait;
-}
