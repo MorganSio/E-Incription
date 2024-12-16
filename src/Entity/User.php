2066acc250a68327cffea9de1d83a1026508abc5
MorganSio <161039598+MorganSio@users.noreply.github.com>
2024-12-16 11:27:06 +0100

    started mdl form

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -90,7 +90,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
         $roles[] = 'ROLE_ELEVE';
-
+ 
         return array_unique($roles);
     }
 
