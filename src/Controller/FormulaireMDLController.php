8928774403d8a187220f0ce4e52546f397a218ff
MorganSio <161039598+MorganSio@users.noreply.github.com>
2025-03-04 16:29:09 +0100

    `Update routes, templates and scripts for MDL and Intendance forms`

--- src/Controller/FormulaireMDLController.php
+++ src/Controller/FormulaireMDLController.php
@@ -10,36 +10,11 @@ use Symfony\Component\HttpFoundation\Request;
 
 class FormulaireMDLController extends AbstractController
 {
-    #[Route('/formulaire_mdl', name: 'formulaire_mdl')]
+    #[Route('/fiche-mdl', name: 'fiche-mdl')]
     public function index(): Response
     {
         return $this->render('index/formulaire_mdl.html.twig', [
             'controller_name' => 'FormulaireMDLController',
         ]);
-    }
-    public function adhesion(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        if ($request->isMethod('POST')) {
-            $accepted = $request->request->get('accepter') === 'oui';
-            $paymentMethod = $request->request->get('payment_method', null);
-            $imageRights = $request->request->get('image_rights', null) === 'authorize';
-
-            if ($accepted) {
-                $adhesion = new Adhesion();
-                $adhesion->setAccepted(true);
-                $adhesion->setPaymentMethod($paymentMethod);
-                $adhesion->setImageRights($imageRights);
-
-                $entityManager->persist($adhesion);
-                $entityManager->flush();
-
-                $this->addFlash('success', 'Votre adhésion a été enregistrée !');
-                return $this->redirectToRoute('formualire_mdl'); // Redirection ou autre route
-            }
-
-            $this->addFlash('error', 'Vous avez refusé d\'adhérer à la MDL.');
-        }
-
-        return $this->render('formulaire_mdl.html.twig');
     }
 }
