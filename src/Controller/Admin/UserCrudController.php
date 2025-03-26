a573b0ed53ad33b291b907681d8d87b0fd9c5788
hugorouff <hugo.rouff@lyceefulbert.fr>
2025-03-26 16:37:59 +0100

    modif docx fiche intendance

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -10,6 +10,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\UrlField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
 
 
 class UserCrudController extends AbstractCrudController
@@ -23,19 +24,22 @@ class UserCrudController extends AbstractCrudController
     {
         return [
             // IdField::new('id')->onlyOnIndex()	,
-            ArrayField::new('roles')->hideOnForm(),
+            IdField::new('id')->onlyOnIndex(),
+            ArrayField::new('email')->hideOnForm(),
             TextField::new('password')->onlyOnForms()->hideOnForm(), // Pour ne pas afficher le mot de passe en clair
             TextField::new('Nom'),
             TextField::new('prenom'),
             BooleanField::new('isVerified')->hideOnForm(),
-            UrlField::new('pdf', 'PDF')
-            ->onlyOnIndex()
-            ->setLabel('PDF')
-            ->setCustomOption('renderAsButton', true)
-            ->setCustomOption('buttonLabel', '<i class="fa fa-file-pdf"></i> Générer PDF')
-            ->formatValue(function ($value, $entity) {
-                return '/admin/generer-pdf/' . $entity->getId();
-            }),
+            // Field::new('pdf', 'Générer PDF')
+            //     ->onlyOnIndex()
+            //     ->formatValue(function ($value, $entity) {
+            //         return sprintf(
+            //             '<a class="btn btn-success" href="%s" target="_blank">
+            //                 <i class="fas fa-file-pdf"></i> Télécharger PDF
+            //             </a>',
+            //             '/admin/generer-docx/' . $entity->getId()
+            //         );
+            //     }),
         ];
     }
 }
