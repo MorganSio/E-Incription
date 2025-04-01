d585d850204f512183eb638b9e76bfab8859ad49
hugorouff <hugo.rouff@lyceefulbert.fr>
2025-04-01 15:38:24 +0200

    modif docx fiche intendance

--- src/Controller/PdfController.php
+++ src/Controller/PdfController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Service\DocxGeneratorService;
+use App\Service\DocxIntendanceGeneratorService;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,21 +15,21 @@ class PdfController extends AbstractController
     //     return $pdfFillerService->generatePdf($id);
     // }
 
-    private DocxGeneratorService $docxGeneratorService;
+    private DocxIntendanceGeneratorService $docxGeneratorService;
 
-    public function __construct(DocxGeneratorService $docxGeneratorService)
+    public function __construct(DocxIntendanceGeneratorService $docxGeneratorService)
     {
 
         $this->docxGeneratorService = $docxGeneratorService;
     }
 
 
-    #[Route('/admin/generer-docx/{id}', name: 'generer_docx')]
+    #[Route('/admin/generer-docx/{id}', name: 'generer_docx_intendance')]
     public function generateDocx(int $id): Response
     {
         try {
             // Appeler la méthode du service pour générer le fichier DOCX
-            return $this->docxGeneratorService->generateDocx($id);
+            return $this->docxIntendanceGeneratorService->generateDocx($id);
         } catch (\Exception $e) {
             // Gestion des erreurs
             return new Response("Erreur lors de la génération du fichier DOCX : " . $e->getMessage(), Response::HTTP_INTERNAL_SERVER_ERROR);
